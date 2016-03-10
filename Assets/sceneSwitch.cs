3d70df9bbd45c9e3df88a96dae439b4a5e0fd024
zhenwei lee <zhenwei2972@hotmail.com>
2016-03-11 03:26:02 +0800

    added main menu and polished scanner UI

---
+++ Assets/sceneSwitch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceneSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void switchPalace()
+    {
+        Application.LoadLevel("HardyScene");
+    }
+    public void switchScanner()
+    {
+        Application.LoadLevel("ARScene");
+    }
+}
