240a7841886ca044dbd665d06c28e773d0cce492
hardyshein <hardyshein@gmail.com>
2016-03-11 03:09:19 +0800

    Mayan Calendar puzzle has been completed.

--- Assets/Scripts/CameraLineTracer.cs
+++ Assets/Scripts/CameraLineTracer.cs
@@ -6,6 +6,9 @@ public class CameraLineTracer : MonoBehaviour
 
     private const float sightDistance = 5;
     private GameObject[] viewedObjects;
+    public bool puzzleMode = false;
+
+    public Vector3 puzzleGazePos;
 
     void Awake()
     {
@@ -33,9 +36,10 @@ public class CameraLineTracer : MonoBehaviour
             // ヒットのオブジェクトのタッグは「Artifact」なら、。。。
             if (hit.transform.gameObject.tag.Equals("Artifact"))
             {
+                #region Regular artifacts
                 // Set the viewed item to "new viewed object"
                 viewedObjects[0] = hit.transform.gameObject;
-                
+
                 //　新しいオブジェクトは前のオブジェクトと同じなら、。。。
                 if (viewedObjects[0] == viewedObjects[1])
                 {
@@ -48,16 +52,59 @@ public class CameraLineTracer : MonoBehaviour
                     // Activate the artifact's display
                     hit.transform.gameObject.GetComponent<ArtifactBehaviour>().Activate();
                 }
+
+
+
+                // Temporarily disable all the colliders on the pieces.
+                GameObject[] pieces = GameObject.FindGameObjectsWithTag("MayanCalendarPiece");
+                foreach (GameObject p in pieces)
+                {
+                    p.GetComponent<PuzzlePiece>().isLookedAt = false;
+                }
+                #endregion
+            }
+            else if (!puzzleMode && hit.transform.gameObject.tag.Equals("MayanCalendarPiece"))
+            {
+                viewedObjects[0] = hit.transform.gameObject;
+
+                if (viewedObjects[0] == viewedObjects[1])
+                {
+
+                }
+                else
+                {
+                    viewedObjects[1] = hit.transform.gameObject;
+                    //puzzleMode = true;
+                    hit.transform.gameObject.GetComponent<PuzzlePiece>().PieceMoveStart();
+                }
+            }
+            else if (puzzleMode && hit.transform.gameObject.tag.Equals("MayanCalendarSupport"))
+            {
+                Debug.Log("Following bah");
+                puzzleGazePos = hit.point;
             }
         }
         else
         {
             // Not looked at anything
             // Deactivate previous looked at item
-            if (viewedObjects[1])
+            if (viewedObjects[1] != null && viewedObjects[1].tag.Equals("Artifact"))
             {
                 viewedObjects[1].GetComponent<ArtifactBehaviour>().Deactivate();
                 viewedObjects[1] = null;
+
+                Debug.Log("LAG");
+            }
+            else if (viewedObjects[1] != null && (viewedObjects[1].tag.Equals("MayanCalendarPiece") || viewedObjects[1].tag.Equals("MayanCalendarSupport")))
+            {
+                // Temporarily disable all the colliders on the pieces.
+                GameObject[] pieces = GameObject.FindGameObjectsWithTag("MayanCalendarPiece");
+                foreach (GameObject p in pieces)
+                {
+                    p.GetComponent<PuzzlePiece>().isLookedAt = false;
+                }
+                viewedObjects[1] = null;
+                Debug.Log("More lag");
             }
         }
     }
